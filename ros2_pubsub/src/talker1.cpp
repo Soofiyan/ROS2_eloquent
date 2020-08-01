f44051145f043cc1cf9b7c9c28805d24571e0f29
Soofiyan Atar <37182533+Soofiyan@users.noreply.github.com>
2020-08-02 01:20:59 +0530

    Adding listener.cpp and talker1.cpp

--- ros2_pubsub/src/talker1.cpp
+++ ros2_pubsub/src/talker1.cpp
@@ -17,9 +17,9 @@ class MinimalPublisher : public rclcpp::Node
     MinimalPublisher()
     : Node("publisher"), count_(0)
     {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-      timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        timer_ = this->create_wall_timer(
+        1ms, std::bind(&MinimalPublisher::timer_callback, this));
     }
 
   private:
@@ -27,7 +27,7 @@ class MinimalPublisher : public rclcpp::Node
     {
       auto message = std_msgs::msg::String();
       message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      RCLCPP_INFO(this->get_logger(), "Publishing in 1 ms: '%s'", message.data.c_str());
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
@@ -38,7 +38,10 @@ class MinimalPublisher : public rclcpp::Node
   int main(int argc, char * argv[])
   {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MinimalPublisher>());
+    while(1)
+    {
+        rclcpp::spin(std::make_shared<MinimalPublisher>());
+    }
     rclcpp::shutdown();
     return 0;
   }
